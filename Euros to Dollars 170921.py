2a8fc94b462cb0367d4e6d4180af00ef1d165861
Michael-Cullen-SKC <95493660+Mcullen22@users.noreply.github.com>
2022-01-12 22:48:01 +0000

    Add files via upload

---
+++ Euros to Dollars 170921.py
@@ -0,0 +1,4 @@
+Euros = float(input('Euros = €'))
+Dollar = float(1.18)
+Result = float(Euros*Dollar)
+print('Dollars: $',Result)
---
+++ Speed.py
@@ -0,0 +1,4 @@
+Distance = int(input('How many kilometres did you travel?: '))
+Time = int(input('How many hours did the journey take?: '))
+Speed = int(Distance / Time)
+print('Your speed is',Speed, 'km/hr')
