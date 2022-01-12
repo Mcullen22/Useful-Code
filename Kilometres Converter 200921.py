01a19bf53ada343c00acc69ec2b9f4d633bc0de3
Michael-Cullen-SKC <95493660+Mcullen22@users.noreply.github.com>
2022-01-12 22:52:49 +0000

    Add files via upload

---
+++ Kilometres Converter 200921.py
@@ -0,0 +1,5 @@
+#A program to convert miles to kilometres
+miles = float(input('Enter the Miles value: '))
+kilometres = miles*1.609
+print(miles, 'Miles equals', kilometres, 'Kilometres ')
+              
